33dce087741c9ad3eb308c39078b597d333deea1
Carl Bush <CarlBushs13@gmail.com>
2022-03-21 11:54:41 -0700

    module exports

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const FileSystem = require("fs");
+const profileDataArgs = process.argv.slice(2);
+const generatePage = require('./src/page-template.js');
+//const profileDataArgs = process.argv.slice(2, process.argv.length);
+//const name = profileDataArgs[0];
+//const github = profileDataArgs[1];
 
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArgs => {
+/*const printProfileData = profileDataArgs => {
     for (let i = 0; i < profileDataArgs.length; i++){
         console.log(profileDataArgs[i]);
     }
@@ -14,4 +19,11 @@ const printProfileData = profileDataArgs => {
     profileDataArgs.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);*/
+
+
+
+FileSystem.writeFile("index.html", generatePage(name, github), err => {
+    if (err) throw err;
+    console.log("Portfolio complete! Check out index.html to see the output!")
+});
