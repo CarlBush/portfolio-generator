d0669e0c59ac62249f8fbb67746577df14a3b8fb
CarlBush <Carl.BushS13@gmail.com>
2022-03-23 15:53:02 -0700

    inquirer validate

--- app.js
+++ app.js
@@ -37,12 +37,30 @@ return inquirer.prompt([
     {
         type: "input",
         name: "name",
-        message: "What is your name?"
+        message: "What is your name? (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            }
+            else{
+                console.log("Please enter your name!");
+                return false;
+            }
+        }
     },
     {
         type: "input",
         name: "github",
-        message: "Enter your GitHub Username"
+        message: "Enter your GitHub Username (Required)",
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            }
+            else{
+                console.log("Please enter your GitHub Username!");
+                return false;
+            }
+        }
     },
     {
         type: "input",
@@ -69,12 +87,30 @@ const promptProject = portfolioData =>{
         {
             type: "input",
             name: "name",
-            message: "What is the name of your project?"
+            message: "What is the name of your project? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else{
+                    console.log("Please enter the name of your project!");
+                    return false;
+                }
+            }
         },
         {
             type: "input",
             name: "description",
-            message: "Provide a description of the project (Required)"
+            message: "Provide a description of the project (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else{
+                    console.log("Provide a description of the project");
+                    return false;
+                }
+            }
         },
         {
             type: "checkbox",
@@ -85,7 +121,16 @@ const promptProject = portfolioData =>{
         {
             type:"input",
             name: "link",
-            message: "Enter the GitHub link to your project. (Required)"
+            message: "Enter the GitHub link to your project. (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else{
+                    console.log("Please enter your GitHub link to your project!");
+                    return false;
+                }
+            }
         },
         {
             type: "confirm",
