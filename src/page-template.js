e673c50ed161895f936531127cbd93a43064afa3
Carl Bush <CarlBushs13@gmail.com>
2022-03-25 11:58:16 -0700

    before adjust generateProjects function

--- src/page-template.js
+++ src/page-template.js
@@ -1,10 +1,11 @@
 //const generatePage =(name, github) => {
 
-//Creating the about section
+//CREATING THE ABOUT SECTION
 //generateAbout(), will accept the about variable as a parameter, 
 // and if it doesn't exist, we'll simply 
 // return an empty string. If it does exist, 
 // however, we'll return the entire <section> element.
+
 const generateAbout = aboutText => {
     if (!aboutText) {
       return '';
@@ -14,6 +15,68 @@ const generateAbout = aboutText => {
       <section class="my-3" id="about">
         <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
         <p>${aboutText}</p>
+      </section>
+    `;
+  };
+
+//GENERATE PROJECTS FUNCTION
+  const generateProjects = projectsArr => {
+    // get array of just featured projects FILTER ARRAY METHOD
+    //IF TRUE we include it in the new array
+    const featuredProjects = projectsArr.filter(project => {
+      if (project.feature) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  
+    // get array of all non-featured projects FILTER ARRAY METHOD
+    const nonFeaturedProjects = projectsArr.filter(project => {
+      if (!project.feature) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  //FUNCTION FOR FEATURED OR NON FEATURED PROJECTS - MAP ARRAY METHOD
+    const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
+      return `
+        <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
+          <h3 class="portfolio-item-title text-light">${name}</h3>
+          <h5 class="portfolio-languages">
+            Built With:
+            ${languages.join(', ')}
+          </h5>
+          <p>${description}</p>
+          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        </div>
+      `;
+    });
+    //FUNCTION FOR FEATURED OR NON FEATURED PROJECTS - MAP ARRAY METHOD
+    const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
+      ({ name, description, languages, link }) => {
+        return `
+          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+      }
+    );
+  
+    return `
+      <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        ${featuredProjectHtmlArr.join('')}
+        ${nonFeaturedProjectHtmlArr.join('')}
+        </div>
       </section>
     `;
   };
@@ -48,6 +111,7 @@ module.exports = templateData => {
       </header>
       <main class="container my-5">
             ${generateAbout(about)}
+            ${generateProjects(projects)}
       </main>
 
       <footer class="container text-center py-3">
